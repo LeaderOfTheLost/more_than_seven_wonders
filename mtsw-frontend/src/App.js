fa4afc7761335cc5d580a08444cd5037480eedcb
David Caldwell <doubledc92@gmail.com>
2022-10-25 10:48:59 -0400

    starting routes

--- mtsw-frontend/src/App.js
+++ mtsw-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 
 function App() { 
   const [wonders, updateWonders] = useState([])
-  const [formState, setFormState] = useState({location: '', description: ''})
+  const [formState, setFormState] = useState({location: '', description: '', img: ''})
+
+  
 
   useEffect(() => {
     const apiCall = async () => {
@@ -19,28 +21,30 @@ function App() {
     const handleChange = (event) => {
       setFormState({...formState, [event.target.id]: event.target.value})
     } 
-
- 
     const handleSubmit = async (event) => {
       event.preventDefault()
-     
+      console.log(formState)
       let newWonder = await axios.post('http://localhost:3001/wonders', formState)
       .then((response) => {
-        return response.data
+        return response
       })
       .catch((error) => {
         console.log(error)
       })
+      console.log(newWonder.data)
       updateWonders([...wonders, newWonder.data])
       setFormState({location: '', description: '', img: ''})
     }
+   
+          
   return (
     <div className="App">
       <h1>All wonders HERE</h1>
       {wonders.map((wonder) => (
         <div key={wonder._id}>
           <h2>{wonder.location}</h2>
-          <img src={wonder.img}></img>
+          <p>{wonder.description}</p>
+          <img src={wonder.img} width= '200px' ></img>
         </div>
       ))} 
       <h3>Add a Wonder</h3> 
@@ -49,7 +53,7 @@ function App() {
           <input id='location' value={formState.location} onChange={handleChange}/>
           <label htmlFor='description'>Description: </label>
           <input id='description' value={formState.description} onChange={handleChange}/>
-          <label htmlFor='img'>Image: </label>
+          <label htmlFor='img'>Image URL: </label>
           <input id='img' value={formState.img} onChange={handleChange}/>
           <button type='submit'>Add Wonder</button>
         </form>
